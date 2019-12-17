d040a2283a7384cae498d854bdae73aa01f54d8d
Adesuwa T. Dawodu <littlezigy@gmail.com>
2019-12-17 02:20:46 +0100

    Testing deploy

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,6 +1,7 @@
 module.exports = {
     loggedin: (req, res, next) => {
         console.log("Checking login object\n\nSEssion==>", req.session);
+        console.log("Checking req.cookies", req.headers);
         try{
             if(req.session.passport.user) {
                 return next();
