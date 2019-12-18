cb8ec8d58377b4e53569056b71489c30a4910502
Adesuwa T. Dawodu <littlezigy@gmail.com>
2019-12-18 20:32:49 +0100

    More cleanup

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,11 +1,11 @@
 module.exports = {
     loggedin: (req, res, next) => {
-        console.log("Checking login object\n\nSEssion==>", req.session);
-        console.log("Checking req.cookies", req.headers);
         try{
+            if(!req.session.passport) return res.failure("Not logged in");
+            
             if(req.session.passport.user) {
                 return next();
-            } else return res.failure("Not logged in");
+            }
         } catch(e) {
             return res.gerror("Error");
         }
