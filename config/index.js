635e6ee4c283a29e62285e42dc8359967a3b056c
Adesuwa T. Dawodu <littlezigy@gmail.com>
2019-12-22 14:28:03 +0100

    Add script to generate test coverage

--- config/index.js
+++ config/index.js
@@ -1,6 +1,6 @@
 config = {}
 
-const cloudinary = require('cloudinary');
+//const cloudinary = require('cloudinary');
 
 if(process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'devtest') {
     config = require("./env/development");
@@ -9,16 +9,10 @@ else if(process.env.NODE_ENV === 'production') config = require("./env/productio
 else if(process.env.NODE_ENV === 'test') config = require("./env/test");
 else config = "No environment specified";
 
-cloudinary.config({
+/*cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET
-});
-
-console.log("CLOUDINARY CONFIG\n------------------------\n",{
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET
-});
+});*/
 
 module.exports = config;
